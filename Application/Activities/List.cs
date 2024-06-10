30e54003d7d57233e2064427e71a8863ea728aaa
Mina <mina.latif@live.com>
2024-06-10 19:38:41 -0400

    code refactor

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,7 @@
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -12,13 +13,29 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            // private readonly ILogger<List> _logger;
+            public Handler(DataContext context/*, ILogger<List> logger*/)
             {
+                // _logger = logger;
                 _context = context;
 
             }
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
+                // try
+                // {
+                //     for (int i = 0; i < 10; i++)
+                //     {
+                //         cancellationToken.ThrowIfCancellationRequested();
+                //         await Task.Delay(1000, cancellationToken);
+                //         _logger.LogInformation($"Task {i} has completed");
+                //     }
+                // }
+                // catch (System.Exception)
+                // {
+                //     _logger.LogInformation("Task was cancelled");
+                // }
+
                 return await _context.Activities.ToListAsync();
             }
         }
