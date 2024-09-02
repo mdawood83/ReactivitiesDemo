c764c07bf4b77c1b920c19db8c1e2c4f534e691d
Mina <mina.latif@live.com>
2024-09-02 19:07:34 -0400

    Added UI Styling

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,87 @@
+import { Link } from "react-router-dom";
+import { Item, Button, Segment, Icon } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+// import { useState, SyntheticEvent } from "react";
+// import { useStore } from "../../../app/stores/store";
+
+interface Props {
+    activity: Activity;
+}
+
+export default function ActivityListItem({activity} : Props) {
+    // const {activityStore} = useStore();
+    // const {deleteActivity, loading} = activityStore;
+
+    // const [target, setTarget] = useState('');
+
+    // function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+    //     setTarget(e.currentTarget.name);
+    //     deleteActivity(id);
+    // }
+    
+  return (
+
+    <Segment.Group>
+        <Segment>
+            <Item.Group>
+                <Item>
+                    <Item.Image size="tiny" circular src="/assets/user.png" />
+                    <Item.Content>
+                        <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                            {activity.title}
+                        </Item.Header>
+                        <Item.Description>
+                            Hosted by Bob
+                        </Item.Description>
+                    </Item.Content>
+                </Item>
+            </Item.Group>
+        </Segment>
+        <Segment>
+            <span>
+                <Icon name="clock" /> {activity.date}
+                <Icon name="marker" /> {activity.venue}
+            </span>
+        </Segment>
+        <Segment secondary>
+            Attendees go here
+        </Segment>
+        <Segment clearing>
+            <span>{activity.description}</span>
+            <Button
+                as={Link}
+                to={`/activities/${activity.id}`}
+                color="teal"
+                floated="right"
+                content="View"
+            />
+        </Segment>
+    </Segment.Group>
+
+    //OLD CODE:
+    // <Item key={activity.id}>
+    //     <Item.Content>
+    //         <Item.Header as='a'>{activity.title}</Item.Header>
+    //         <Item.Meta>{activity.date}</Item.Meta>
+    //         <Item.Description>
+    //             <div>{activity.description}</div>
+    //             <div>{activity.city}, {activity.venue}</div>
+    //         </Item.Description>
+    //         <Item.Extra>
+    //             {/* <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content="View" color="blue" /> */}
+    //             <Button as={Link} to={`/activities/${activity.id}`} floated="right" content="View" color="blue" />
+    //             <Button
+    //                 name={activity.id}
+    //                 loading={loading && target === activity.id}
+    //                 onClick={(e) => handleActivityDelete(e, activity.id)}
+    //                 floated="right"
+    //                 content="Delete"
+    //                 color="red"
+    //             />
+    //             {/* <Label onClick={cancelActivity} basic content={activity.category} /> */}
+    //             <Label basic content={activity.category} />
+    //         </Item.Extra>
+    //     </Item.Content>
+    // </Item>
+  )
+}
