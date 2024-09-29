cc47d63577374065ef238ee0fc8d9f44eca8756c
Mina <mina.latif@live.com>
2024-09-28 21:11:49 -0400

    API Image upload

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -13,10 +13,14 @@ namespace Application.Core
                 .ForMember(d => d.HostUsername,
                     opt => opt.MapFrom(src => src.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
             
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(d => d.DisplayName, opt => opt.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(d => d.Username, opt => opt.MapFrom(src => src.AppUser.UserName))
-                .ForMember(d => d.Bio, opt => opt.MapFrom(src => src.AppUser.Bio));
+                .ForMember(d => d.Bio, opt => opt.MapFrom(src => src.AppUser.Bio))
+                .ForMember(d => d.Image, opt => opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+            
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
